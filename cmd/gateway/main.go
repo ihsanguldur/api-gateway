f8bb7ca904001e205c8ff6332bc131f2a97ddd6a
Ihsan Guldur <ihsanguldur@outlook.com>
2026-09-21 11:49:25 +0300

    Add per-client-IP token bucket rate limiting middleware

--- cmd/gateway/main.go
+++ cmd/gateway/main.go
@@ -9,6 +9,7 @@ import (
 	"github.com/ihsanguldur/api-gateway/internal/health"
 	"github.com/ihsanguldur/api-gateway/internal/loadbalancer"
 	"github.com/ihsanguldur/api-gateway/internal/proxy"
+	"github.com/ihsanguldur/api-gateway/internal/ratelimit"
 	"github.com/ihsanguldur/api-gateway/internal/registry"
 	"github.com/ihsanguldur/api-gateway/internal/router"
 )
@@ -17,6 +18,9 @@ const (
 	backendTTL     = 10 * time.Second
 	sweepInternal  = 5 * time.Second
 	healthInterval = 2 * time.Second
+	rateLimit      = 10
+	rateBurst      = 20
+	limiterSweep   = 1 * time.Minute
 )
 
 func main() {
@@ -29,6 +33,10 @@ func main() {
 	checker := health.NewChecker(reg)
 	checker.Start(healthInterval, nil)
 
+	// TODO: hardcoded for now, will be moved to a JSON config file.
+	limiter := ratelimit.New(rateLimit, rateBurst)
+	limiter.StartJanitor(limiterSweep, nil)
+
 	mux := http.NewServeMux()
 	reg.RegisterRoutes(mux)
 
@@ -37,7 +45,7 @@ func main() {
 		{Prefix: "/api/users", Service: "user-service", LB: &loadbalancer.LeastConnections{}},
 		{Prefix: "/api/orders", Service: "order-service", LB: &loadbalancer.WeightedRoundRobin{}},
 	})
-	mux.Handle("/", proxy.NewBalancedProxy(rt, reg))
+	mux.Handle("/", limiter.Middleware(proxy.NewBalancedProxy(rt, reg)))
 
 	log.Printf("gateway listening on %s", *addr)
 	if err := http.ListenAndServe(*addr, mux); err != nil {
